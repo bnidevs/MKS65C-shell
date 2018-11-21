13578a2451793901420f088bae5d44684fa88415
Bill Ni <bni@stuy.edu>
2018-11-20 20:06:10 -0500

    prompt working

--- shell.c
+++ shell.c
@@ -21,10 +21,43 @@ void exec(char * line){
 	int f = fork();
 	if(!f){
 		execvp(array[0], array);
+	}else{
+		wait(NULL);
 	}
 	free(array);
 }
 
+void prompt(){
+
+	//hostname
+
+	char * host = malloc(64);
+	gethostname(host, 64);
+	char * cut = strchr(host, 46);
+	*cut = 0;
+
+	//currdir
+
+	char * ccwd = malloc(128);
+	getcwd(ccwd, 128);
+	cut = strchr(ccwd, 0);
+	while(!strchr(cut - 1, 47)){
+		cut--;
+	}
+	char * cwd = cut;
+
+	//usrname
+
+	char * usr = malloc(16);
+	getlogin_r(usr, 16);
+
+	printf("%s:%s %s$ ", host, cwd, usr);
+	free(host);
+	free(ccwd);
+	free(usr);
+}
+
 int main(){
+	prompt();
 	return 0;
 }

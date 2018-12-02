a2ec885aabb45e611ae4a12d1fbff4d8ae8d97e1
Bill Ni <bni@stuy.edu>
2018-12-01 23:55:12 -0500

    Update shell.c

--- shell.c
+++ shell.c
@@ -3,6 +3,7 @@
 #include <unistd.h>
 #include <sys/wait.h>
 #include <string.h>
+#include <fcntl.h>
 
 char * strip(char * material){
 	while(!strncmp(material, " ", 1)){
@@ -36,22 +37,56 @@ void print_arr(char ** args) {
 }
 
 void exec(char * line){
-	char ** array = pargs(line, " ");
+	if(strchr(line, 62)){
+		char ** array = pargs(line, ">");
 
-	if(!strcmp(array[0], "cd")){
-		chdir(array[1]);
-	}else if(!strcmp(array[0], "exit")){
-		exit(0);
-	}else{
 		int f = fork();
 		if(!f){
-			execvp(array[0], array);
+			int fd = open(array[1], O_CREAT | O_WRONLY, 0777);
+			dup2(fd, 1);
+			char ** cmdarray = pargs(array[0], " ");
+			execvp(cmdarray[0], cmdarray);
+			free(cmdarray);
 			exit(0);
 		}else{
 			wait(NULL);
 		}
+
+		free(array);
+	}else if(strchr(line, 60)){
+		char ** array = pargs(line, "<");
+
+		int f = fork();
+		if(!f){
+			int fd = open(array[1], O_CREAT | O_RDONLY, 0777);
+			dup2(fd, 0);
+			char ** cmdarray = pargs(array[0], " ");
+			execvp(cmdarray[0], cmdarray);
+			free(cmdarray);
+			exit(0);
+		}else{
+			wait(NULL);
+		}
+
+		free(array);
+	}else{
+		char ** array = pargs(line, " ");
+
+		if(!strcmp(array[0], "cd")){
+			chdir(array[1]);
+		}else if(!strcmp(array[0], "exit")){
+			exit(0);
+		}else{
+			int f = fork();
+			if(!f){
+				execvp(array[0], array);
+				exit(0);
+			}else{
+				wait(NULL);
+			}
+		}
+		free(array);
 	}
-	free(array);
 }
 
 void prompt(){
